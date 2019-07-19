a09124031b3392dc523d8d01a8fbb8285db8c11d
acl <acl447@nyu.edu>
2019-07-18 20:36:07 -0700

    NewPost component, Nav component imported to index.js

---
+++ src/components/NewPost.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Col, Row, Container} from "./Grid/index";
+
+import { Input, TextArea, FormBtn } from "./Form/index";
+
+function NewPost() {
+    return (
+        
+        <Container fluid>
+       <Row>
+         <Col size="md-6">
+           
+           <form>
+             <Input
+               value={this.state.title}
+               onChange={this.handleInputChange}
+               name="title"
+               placeholder="Title (required)"
+             />
+             <Input
+               value={this.state.author}
+               onChange={this.handleInputChange}
+               name="author"
+               placeholder="Author (required)"
+             />
+             <TextArea
+               value={this.state.synopsis}
+               onChange={this.handleInputChange}
+               name="synopsis"
+               placeholder="Synopsis (Optional)"
+             />
+             <FormBtn
+               disabled={!(this.state.author && this.state.title)}
+               onClick={this.handleFormSubmit}
+             >
+               Submit Book
+             </FormBtn>
+           </form>
+         </Col>
+         <Col size="md-6 sm-12">
+           <Jumbotron>
+             <h1>Books On My List</h1>
+           </Jumbotron>
+           {this.state.books.length ? (
+             <List>
+               {this.state.books.map(book => (
+                 <ListItem key={book._id}>
+                   <Link to={"/books/" + book._id}>
+                     <strong>
+                       {book.title} by {book.author}
+                     </strong>
+                   </Link>
+                   <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                 </ListItem>
+               ))}
+             </List>
+           ) : (
+             <h3>No Results to Display</h3>
+           )}
+         </Col>
+       </Row>
+     </Container>
+
+      
+       
+    );
+   }
+   
+   export default NewPost;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/home';
 import DashboardPage from './pages/dashboard';
@@ -9,28 +9,32 @@ import Auth from './components/Auth';
 import AllPostsPage from './pages/allposts';
 import AboutPage from './pages/about';
 import InboxPage from './pages/inbox';
+// import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App container">
-        <Auth>
-      <div className="jumbotron">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/about" component={AboutPage} />
-            <Route path="/inbox" component={InboxPage} />
-            <Route path="/dashboard" component={DashboardPage}/>
-            <Route path="/callback" component={CallbackPage} />
-            <Route path="/allposts" component={AllPostsPage} />
-          </Switch>
-        </Router>
+      <Auth>
+        <div className="jumbotron">
+          <Router>
+            <div>
+              {/* <Nav /> */}
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/inbox" component={InboxPage} />
+                <Route path="/dashboard" component={DashboardPage} />
+                <Route path="/callback" component={CallbackPage} />
+                <Route path="/allposts" component={AllPostsPage} />
+              </Switch>
+            </div>
+          </Router>
         </div>
-        </Auth>
-      
+      </Auth>
+
     </div>
   );
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App/>, rootElement);
+ReactDOM.render(<App />, rootElement);
