1f301010f6fd1f29897ccbc3e599337efd9a384e
acl <acl447@nyu.edu>
2019-07-22 21:30:27 -0700

    removed Link

--- client/src/pages/allposts.js
+++ client/src/pages/allposts.js
@@ -4,8 +4,8 @@ import NavTabs from "../components/Navbar/NavTabs";
 import { AuthConsumer } from "../authContext";
 import Can from "../components/Can";
 // import Logout from "../components/Logout";
-// import PostsList from "../components/PostsList.js";
-import { List, ListItem } from "../components/List";
+import PostsList from "../components/PostsList.js";
+// import { List, ListItem } from "../components/List";
 
 const AllPostsPage = () => (
   <AuthConsumer>
@@ -18,23 +18,8 @@ const AllPostsPage = () => (
             <NavTabs />
             <h1>All Posts</h1>
             {/* <Logout /> */}
-            {/* <PostsList /> */}
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/posts/" + post._id}>
-                      <strong>
-                        {post.text} by {book.author}
-                      </strong>
-                    </Link>
-                    {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
+            <PostsList />
+           
             
           </div>
         )}
