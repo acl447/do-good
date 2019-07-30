10be7666b2f8590288c6dcb7bacf1c4fcf742a3b
acl <acl447@nyu.edu>
2019-07-30 14:18:00 -0700

    submit button positioning

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -16,7 +16,7 @@ export function TextArea(props) {
 }
 export function FormBtn(props) {
  return (
-   <button {...props} style={{ marginTop: 305, marginLeft: 123 }} className="btn btn-primary btn-sm">
+   <button {...props} style={{ marginTop: 340, marginLeft: 123 }} className="btn btn-primary btn-sm">
      {props.children}
    </button>
  );
